75d321447d666db27357de6d75a87112a61ca895
巴宏斌 <bahb@mochasoft.com.cn>
2022-07-17 10:50:14 +0800

    [feat] Git Commit Template 功能。 #NONE-0
    1、完成 Git Commit Template 功能；
    2、功能说明。

---
+++ README.md
@@ -0,0 +1,3 @@
+## Git Commit Template - Web
+
+用于为不同开发工具提供统一的 Git Commit 模板。
---
+++ js/main.js
@@ -0,0 +1,63 @@
+(function () {
+
+    var dataObj = {
+        changeType: '',
+        changeScope: '',
+        shortDesc: '',
+        jiraIssueID: '',
+        longDesc: '',
+        breakingChanges: '',
+        closedIssues: ''
+    };
+
+    var vm = new Vue({
+        el: '#app',
+        data: dataObj,
+        methods: {
+            copyToClipboard() {
+                // alert(Clipboard);
+                // alert('abc');
+                navigator.clipboard.writeText(this.commitMessage).then(function () {
+                    /* clipboard successfully set */
+                    alert('已复制')
+                }, function () {
+                    /* clipboard write failed */
+                    alert('复制失败')
+                });
+            }
+        },
+        computed: {
+            changeScopeDisplay() {
+                return this.changeScope.trim() === '' ? '' : '(' + this.changeScope + ')'
+            },
+            breakingChangesDisplay() {
+                return this.breakingChanges.trim() === '' ? '' : 'BREAKING CHANGE: ' + this.breakingChanges
+            },
+            closedIssuesDisplay() {
+                var retStr = '';
+                if (this.closedIssues.trim() != '') {
+                    var issueArr = this.closedIssues.split(',')
+                    issueArr.forEach(element => {
+                        retStr += '\nCloses #' + element;
+                    });
+                }
+                return retStr;
+            },
+            jiraIssueIDDisplay() {
+                return this.jiraIssueID.trim() === '' ? '#NONE-0' : this.jiraIssueID;
+            },
+            commitMessage() {
+                var commitMsg = `[${this.changeType}${this.changeScopeDisplay}] ${this.shortDesc} ${this.jiraIssueIDDisplay}
+
+${this.longDesc}
+
+${this.breakingChangesDisplay}
+${this.closedIssuesDisplay}
+`
+                return commitMsg.trim();
+            }
+        }
+    });
+
+
+})()
